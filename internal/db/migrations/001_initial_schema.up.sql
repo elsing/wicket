e0cdcf83394a2e3d218df7ac427749fea644235b
Elliot Singer <elliot@singermail.uk>
2026-04-13 00:44:08 +0100

    test

--- internal/db/migrations/001_initial_schema.up.sql
+++ internal/db/migrations/001_initial_schema.up.sql
@@ -165,11 +165,17 @@ CREATE TABLE local_admins (
 
 CREATE TABLE groups_endpoint_override AS SELECT id, ''::TEXT AS endpoint_override FROM groups WHERE FALSE;
 
--- updated_at triggers
+-- +migrate StatementBegin
 CREATE OR REPLACE FUNCTION update_updated_at()
-RETURNS TRIGGER AS $$
-BEGIN NEW.updated_at = NOW(); RETURN NEW; END;
-$$ LANGUAGE plpgsql;
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $func$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$func$;
+-- +migrate StatementEnd
 
 CREATE TRIGGER groups_updated_at  BEFORE UPDATE ON groups  FOR EACH ROW EXECUTE FUNCTION update_updated_at();
 CREATE TRIGGER subnets_updated_at BEFORE UPDATE ON subnets FOR EACH ROW EXECUTE FUNCTION update_updated_at();
